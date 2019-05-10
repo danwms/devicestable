5b677bb9ddba694d69c8434c1a81a1ad90cef279
danwms <me@danwms.co.uk>
2019-05-10 16:51:06 +0100

    Updated to display devices

--- app/table-overview-example.ts
+++ app/table-overview-example.ts
@@ -1,19 +1,16 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
-export interface UserData {
+export interface DeviceData {
   id: string;
   name: string;
-  progress: string;
-  color: string;
+  deviceType: string;
+  site: string;
+  group: string;
+  maxImport: number;
+  maxExport: number;
+  organisation: string;
 }
-
-/** Constants used to fill up our data base. */
-const COLORS: string[] = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
-  'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
-const NAMES: string[] = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
-  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
-  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
 
 /**
  * @title Data table with sorting, pagination, and filtering.
@@ -24,18 +21,116 @@ const NAMES: string[] = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
   templateUrl: 'table-overview-example.html',
 })
 export class TableOverviewExample implements OnInit {
-  displayedColumns: string[] = ['id', 'name', 'progress', 'color'];
-  dataSource: MatTableDataSource<UserData>;
+  displayedColumns: string[] = ['deviceType', 'id', 'name', 'site',  'group', 'maxImport', 'maxExport', 'organisation'];
+  dataSource: MatTableDataSource<DeviceData>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor() {
-    // Create 100 users
-    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+    const devices = [
+    {
+      id: '0003-4000-800e-000000000004',
+      name: 'CH BYD1',
+      deviceType: 'LSES',
+      site: 'ClayHill',
+      group: '',
+      maxImport: -13000,
+      maxExport: 13000,
+      organisation: 'EDF'
+    },
+    {
+      id: '0003-4000-800e-000000000005',
+      name: 'CH BYD2',
+      deviceType: 'LSES',
+      site: 'ClayHill',
+      group: '',
+      maxImport: -13000,
+      maxExport: 13000,
+      organisation: 'EDF'
+    },
+    {
+      id: '0003-4000-800e-000000000006',
+      name: 'CH BYD3',
+      deviceType: 'LSES',
+      site: 'ClayHill',
+      group: '',
+      maxImport: -13000,
+      maxExport: 13000,
+      organisation: 'EDF'
+    }
+    ,{
+      id: '0003-4000-800e-000000000007',
+      name: 'CH BYD4',
+      deviceType: 'LSES',
+      site: 'ClayHill',
+      group: '',
+      maxImport: -13000,
+      maxExport: 13000,
+      organisation: 'EDF'
+    },{
+      id: '0003-4000-800e-000000000008',
+      name: 'CH BYD5',
+      deviceType: 'LSES',
+      site: 'ClayHill',
+      group: '',
+      maxImport: -13000,
+      maxExport: 13000,
+      organisation: 'EDF'
+    },{
+      id: '00000000-0003-4000-8007-000000000002',
+      name: 'Willenhall',
+      deviceType: 'LSES',
+      site: 'Willenhall',
+      group: '',
+      maxImport: -1100000,
+      maxExport: 550000,
+      organisation: 'Sheffield Uni'
+    },
+    {
+      id: '0003-4000-800f-000000000001',
+      name: 'Alpha ESS',
+      deviceType: 'LSES',
+      site: 'BB Science Park',
+      group: '',
+      maxImport: 0,
+      maxExport: 50000,
+      organisation: 'Total'
+    },
+    {
+      id: '0006-4000-8008-000000000000',
+      name: 'Gen 1',
+      deviceType: 'Generator',
+      site: 'Sandy Heath',
+      group: 'Generators',
+      maxImport: 0,
+      maxExport: 50000,
+      organisation: 'Arqiva'
+    },
+    {
+      id: '0006-4000-8009-000000000000',
+      name: 'T 1',
+      deviceType: 'Peaker',
+      site: 'Spear Street',
+      group: 'Test',
+      maxImport: 0,
+      maxExport: 50000,
+      organisation: 'Upside'
+    },
+    {
+      id: '0006-4000-8009-000000000010',
+      name: 'Big Gen',
+      deviceType: 'Generator',
+      site: 'Bacton',
+      group: 'Main',
+      maxImport: 0,
+      maxExport: 5000000,
+      organisation: 'Shell'
+    }
+    ];
 
     // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
+    this.dataSource = new MatTableDataSource(devices);
   }
 
   ngOnInit() {
@@ -50,20 +145,45 @@ export class TableOverviewExample implements OnInit {
       this.dataSource.paginator.firstPage();
     }
   }
-}
 
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name =
-      NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
 
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
-  };
+
+  getTypeIcon(type: string) : string {
+    if (type === 'LSES'){
+      return 'battery_charging_full'
+    } 
+    else if (type === "Generator") {
+      return 'flash_on'
+    }
+    else {
+      return 'help'
+    }
+  }
+
+getOrgIcon(type: string) : string {
+    if (type === 'EDF'){
+      return 'https://dwkujuq9vpuly.cloudfront.net/news/wp-content/uploads/2017/12/EDF-960x480.jpg'
+    } 
+    else if (type === "Upside") {
+      return 'https://www.powerengineeringint.com/content/dam/pei/online-articles/2018/01/upside.png'
+    }
+    else if (type === "Arqiva") {
+      return 'https://www.tvbeurope.com/wp-content/uploads/2019/01/arqiva.jpg'
+    }
+    else if (type === "Sheffield Uni") {
+      return 'https://upload.wikimedia.org/wikipedia/en/4/44/University_of_Sheffield_coat_of_arms.png'
+    }
+    else if (type === "Total") {
+      return 'https://www.pressandjournal.co.uk/wp-content/uploads/sites/2/2018/07/5b485080a4e2b-372x372.jpg'
+    }
+    else if (type === "Shell") {
+      return 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e8/Shell_logo.svg/1200px-Shell_logo.svg.png'
+    }
+    else {
+      return ''
+    }
+  }
+
 }
 
 
